3498bd2137f6a94e41e974883b0bba1678fe9eb0
Atul Yadav <atuly3051@gmail.comm>
2024-08-13 10:22:02 +0530

    Updated App.Js

--- src/App.js
+++ src/App.js
@@ -18,47 +18,43 @@ const createCard = (data)=>{
 }
 
 
-//Test 
 
-const check = (val)=>{
+const menu = (val)=>{
 
-      if(val===true)
-      {
-        return(
+      return(
           <div className='menu'>
             <h2>Our menu</h2>
             <p>Authentic Italian cuisine. 6 creative dishes to choose from all from one oven. All organic, All delicious</p>
             <div className='pizzas'>
             {pizzaData.map(createCard)}
            </div>
-      </div>
-        )
-      }
-      else 
-      {
-        return(
-          <>
-          
+           </div>
+)
+}
+const In_progess = ()=>
+{
+  return(
+       <>
           <p>
             Currently working on the Menu. Be sure to Visit us Later :
           </p>
           <div className='order'><button className='btn'>Order</button></div>
-          
-          </>
-        )
-      }
+         </> 
+  )
+
 }
+
 
 let time= new Date().getHours();
 
 function App() {
   let open_hours = false;
-  if(time<12 && time <20) open_hours = true;
+  if(time<12 && time >20) open_hours = true;
   
   return (
     <div className="container">
       <header> <h1>PIZZA HUB</h1> </header>
-      {check(open_hours)}
+      {open_hours? menu():In_progess()}
       <footer>We are Happy to serve to Between 12:00 and 20:00. Come Visit us or Order Online</footer>
     </div>
   );
