ab0ba838e336258fe79f7b88c361f2853470f0e0
Atul Yadav <atuly3051@gmail.comm>
2024-09-29 12:39:40 +0530

    Updated time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-import { Menu ,In_progess} from './components/menu';
-
-
-let time= new Date().getHours();
+import "./App.css";
+import { Menu, In_progess } from "./components/menu";
+let time = new Date().getHours();
 
 function App() {
   let open_hours = false;
-  if(time<12 && time <20) open_hours = true;
-  
+  if (time >= 12 && time <= 20) open_hours = true;
+
   return (
     <div className="container">
+      <header>
+        {" "}
+        <h1>PIZZA HUB</h1>{" "}
+      </header>
+      {open_hours ? <Menu /> : <In_progess />}
+      <footer>
+        We are Happy to serve to Between 12:00 and 20:00. Come Visit us or Order
+        Online
+      </footer>
       
-      <header> <h1>PIZZA HUB</h1> </header>
-      {open_hours? <Menu/> :<In_progess/>}
-      <footer>We are Happy to serve to Between 12:00 and 20:00. Come Visit us or Order Online</footer>
     </div>
   );
 }
-
-
 
 export default App;
